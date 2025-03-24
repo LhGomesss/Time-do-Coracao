01435f1d67deb9f0337da281467c56a542ec9562
LhGomesss <luishfeitosa01@gmail.com>
2025-03-24 07:52:02 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write  (" S A N ");
+
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write (" T O S ");
